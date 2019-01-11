0e4a263e19c2c400d10e48bb8f099806b3cc2eb1
Elvanos <xfenix@seznam.cz>
2019-01-11 02:16:59 +0100

    added redux

--- .history/src/app_20170120050710.jsx
+++ .history/src/app_20170120050710.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (<div>
-      <h2>Welcome to Reactos!</h2>
+      <h2>Welcome to React!</h2>
     </div>);
   }
 }
